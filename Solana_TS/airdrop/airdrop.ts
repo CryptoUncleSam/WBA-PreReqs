804ac49479202cbf102f2dc6b5310fa816a2258c
CryptoUncleSam <tiosam@DESKTOP-L0BRGT6>
2023-04-28 19:56:50 +0200

    change explorer to solscan

--- Solana_TS/airdrop/airdrop.ts
+++ Solana_TS/airdrop/airdrop.ts
@@ -12,7 +12,7 @@ const connection = new Connection("https://api.devnet.solana.com");
         // We're going to claim 2 devnet SOL tokens
         const txhash = await connection.requestAirdrop(keypair.publicKey, 2 * LAMPORTS_PER_SOL);
         console.log(`Success! Check out your TX here: 
-        https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+        https://solscan.io/tx/${txhash}?cluster=devnet`);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
